f801fc383eaca41cf2b67c7857489925151da2c7
Murilo Silva <murilo.alex.silva@gmail.com>
2020-10-31 14:38:40 -0300

    .

--- lib/pages/HomePage/HomePage.dart
+++ lib/pages/HomePage/HomePage.dart
@@ -55,6 +55,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: CustomFloatingButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: CustomBottomAppBar(),
+      //
     );
   }
 }
